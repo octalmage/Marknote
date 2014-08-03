58940e053cecf22ecce5a9e9b7d9c819bd39ccbb
Jason Stallings <jason@stallin.gs>
2014-08-03 02:18:30 -0500

    Added syntax highlighting support inside notes.

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 var marked = require('marked');
+var highlight = require('highlight.js');
+
+
+
+//Custom Renderer
+var renderer = new marked.Renderer();
+
+//Custom links, going to use for internal links!
+renderer.link = function (href, title, text) 
+{
+	output="<a target=\"_blank\" style='color:pink;' href=\"" + href + "\">" + text + "</a>";
+
+	return output;
+}
+
+/*
+renderer.code = function (code, language)
+{
+	output="<div id=\"code\">" + highlight.highlightAuto(code).value + "</div>";
+	return output;
+
+}
+*/
+
+//Markdown Settings. Using Github styled markdown for automatic links, code blocks, and tables. 
+marked.setOptions({
+  renderer: renderer,
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: true,
+  highlight: function (code)  //use highlight.js for syntax highlighting. 
+  {
+  	console.log(code);
+  	console.log(highlight.highlightAuto(code).value);
+    return highlight.highlightAuto(code).value;
+  }
+});
+
 note="#I am using#\n**markdown**.";
-markdown=marked(note);
+markdown=marked(note, { renderer: renderer });
 
 var store=new Lawnchair(function()
 {
@@ -21,7 +63,7 @@ $(document).on("ready",function()
 	var win = gui.Window.get();
 
 	//Show Chrome debug console.
-	//win.showDevTools();
+	win.showDevTools();
 
 	$("#display").html(markdown);
 
