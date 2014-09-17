f6b7d03f403baeb6eed30f8a53fcf338712e4c23
Jason Stallings <jason@stallin.gs>
2014-09-17 02:04:33 -0500

    Removed unnecessary code.

--- app.js
+++ app.js
@@ -17,44 +17,6 @@ var Private_Parse_Notes = new Parse_Notes();
 var syncing;
 var parsenoteid;
 
-/*
-
-    
-var user = Parse.User.logIn("username", "password", 
-{
-	success: function(user) 
-  	{
-  		console.log(user);
- 	}
-
-});
-
-
-
-
-var user = new Parse.User();
-user.set("username", "my name");
-user.set("password", "my pass");
-user.set("email", "email@example.com");
- 
-// other fields can be set just like with Parse.Object
-user.set("phone", "415-392-0202");
- 
-user.signUp(null, {
-  success: function(user) {
-    // Hooray! Let them use the app now.
-  },
-  error: function(user, error) {
-    // Show the error message somewhere and let the user try again.
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
-});*/
-
-
-
-
-
 var marked = require('marked');
 var highlight = require('highlight.js');
 var editor;
