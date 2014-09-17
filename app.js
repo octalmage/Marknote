f6b605daad07b34b68bce46e1522a082332282d2
Jason Stallings <jason@stallin.gs>
2014-09-17 02:04:53 -0500

    Added comment for parse variables.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ if (process.platform=="darwin")
 	win.menu = mb;
 }
 
+//Parse Variables.
 Parse.initialize("VvmNgHcupWn43L9ThaNDiIldMSjOXiLvd7DR7wTq", "DTAv28KMYt5pYlY3Q1yDJ3Tvm2FOViL4io9deBBt");
 var Parse_Notes = Parse.Object.extend("Notes");
 var Private_Parse_Notes = new Parse_Notes();
