d7169a7c551909eb5b2c6ec2c10778bdf57c1987
Jason Stallings <jason@stallin.gs>
2015-02-02 12:21:06 -0600

    zip 32bit builds for OS X and Windows.

--- tests/before_deploy.sh
+++ tests/before_deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd build/Marknote/osx/
+cd build/Marknote/osx32/
 zip -r MarknoteMac.zip ./*
-cd ../win/
+cd ../win32/
 zip -r MarknoteWin.zip ./*
 cd ../linux64/
 zip -r MarknoteLinux.zip ./*
 cd ../../../
-mv build/Marknote/osx/MarknoteMac.zip ./
-mv build/Marknote/win/MarknoteWin.zip ./
+mv build/Marknote/osx32/MarknoteMac.zip ./
+mv build/Marknote/win32/MarknoteWin.zip ./
 mv build/Marknote/linux64/MarknoteLinux.zip ./
