2c7117cb188adff64fda758a3dcd62184f3176ea
Jason Stallings <jason@stallin.gs>
2015-03-24 18:36:45 -0500

    Updated deploy script for new builds.

--- tests/before_deploy.sh
+++ tests/before_deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd build/Marknote/osx32/
+cd build/Marknote/osx64/
 zip -r MarknoteMac.zip ./*
 cd ../win32/
 zip -r MarknoteWin.zip ./*
-cd ../linux64/
+cd ../linux32/
 zip -r MarknoteLinux.zip ./*
 cd ../../../
-mv build/Marknote/osx32/MarknoteMac.zip ./
+mv build/Marknote/osx64/MarknoteMac.zip ./
 mv build/Marknote/win32/MarknoteWin.zip ./
-mv build/Marknote/linux64/MarknoteLinux.zip ./
+mv build/Marknote/linux32/MarknoteLinux.zip ./
